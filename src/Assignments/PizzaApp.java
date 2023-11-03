6a41d23bd065ae8da1613bc6b17fb216b22a89d9
Melody Oluchi <melodyoluchi848@gmail.com>
2023-11-03 23:08:21 +0100

    assignment

---
+++ src/Assignments/PizzaApp.java
@@ -0,0 +1,161 @@
+package Assignments;
+import javax.xml.stream.events.NotationDeclaration;
+import java.util.Scanner;
+
+public class PizzaApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(""" 
+                <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+                    WELCOME TO MELLY'S PIZZA APP!!
+                <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>   
+                """);
+
+        while (true) {
+            System.out.println("Can i take your order? \n (Yes or No) ");
+            String response = input.nextLine();
+
+            if (response.equalsIgnoreCase("Yes")) {
+                System.out.println("What size of pizza do you want to buy? \n (Large, Medium or Small)\n ");
+                String sizeOfPizza = input.nextLine();
+
+                if (sizeOfPizza.equalsIgnoreCase("Large")) {
+                    System.out.println("What number of guests are you expecting? ");
+                    int numberOfGuestForLargePizza = input.nextInt();
+
+                    if (numberOfGuestForLargePizza > 0) {
+                        System.out.println("How many people are super hungry from the " + numberOfGuestForLargePizza + " guests you are expecting? ");
+                        int superHungryPeopleForLargePizza = input.nextInt();
+
+                        if (superHungryPeopleForLargePizza >= 0 && superHungryPeopleForLargePizza <= numberOfGuestForLargePizza) {
+                            System.out.println("How many slices will each super hungry guest eat? ");
+                            int slicesPerSuperHungry = input.nextInt();
+
+                            System.out.println("How many people are hungry from the " + (numberOfGuestForLargePizza - superHungryPeopleForLargePizza) + " guests left?");
+                            int hungryPeopleForLargePizza = input.nextInt();
+
+                            if (hungryPeopleForLargePizza >= 0 && hungryPeopleForLargePizza <= (numberOfGuestForLargePizza - superHungryPeopleForLargePizza)) {
+                                System.out.println("How many slices will each hungry guest eat?");
+                                int slicesPerHungry = input.nextInt();
+
+                                int notSoHungryPeopleForLargePizza = numberOfGuestForLargePizza - superHungryPeopleForLargePizza - hungryPeopleForLargePizza;
+
+                                System.out.println("How many slices will each not-so-hungry guest eat?");
+                                int slicesPerNotSoHungry = input.nextInt();
+
+                                int largePizzaSlices = 10;
+
+                                int totalSlicesNeeded = (superHungryPeopleForLargePizza * slicesPerSuperHungry) +
+                                        (hungryPeopleForLargePizza * slicesPerHungry) +
+                                        (notSoHungryPeopleForLargePizza * slicesPerNotSoHungry);
+
+
+                                int largePizzaBoxes = (totalSlicesNeeded + largePizzaSlices - 1) / largePizzaSlices;
+                                int remainingSlices = largePizzaSlices - slicesPerSuperHungry - slicesPerHungry - slicesPerNotSoHungry;
+                                ;
+
+                                System.out.println("You will need about " + largePizzaBoxes + " boxes of pizza. \n");
+                                System.out.println("Approximately " + remainingSlices + " slices of pizza will remain.");
+
+                            }
+                        }
+                    } else if (numberOfGuestForLargePizza < 0) {
+                        System.out.println("Give a valid number of guests!. \n Try Again.\n");
+                    }
+
+                } else if (sizeOfPizza.equalsIgnoreCase("Medium")) {
+                    System.out.println("What number of guests are you expecting? ");
+                    int numberOfGuestForMediumPizza = input.nextInt();
+
+                    if (numberOfGuestForMediumPizza > 0) {
+                        System.out.println("How many guests are hungry from the " + numberOfGuestForMediumPizza + " guests you are expecting? ");
+                        int superHungryPeopleForMediumPizza = input.nextInt();
+
+                        if (superHungryPeopleForMediumPizza >= 0 && superHungryPeopleForMediumPizza <= numberOfGuestForMediumPizza) {
+                            System.out.println("How many slices will each super hungry guest eat?");
+                            int slicesPerSuperHungry = input.nextInt();
+
+                            System.out.println("How many people are hungry from the " + (numberOfGuestForMediumPizza - superHungryPeopleForMediumPizza) + " guests left?");
+                            int hungryPeopleForMediumPizza = input.nextInt();
+
+                            if (hungryPeopleForMediumPizza >= 0 && hungryPeopleForMediumPizza <= (numberOfGuestForMediumPizza - superHungryPeopleForMediumPizza)) {
+                                System.out.println("How many slices will each hungry guest eat?");
+                                int slicesPerHungry = input.nextInt();
+
+                                int notSoHungryPeopleForMediumPizza = numberOfGuestForMediumPizza - superHungryPeopleForMediumPizza - hungryPeopleForMediumPizza;
+
+                                System.out.println("How many slices will each not-so-hungry guest eat?");
+                                int slicesPerNotSoHungry = input.nextInt();
+
+                                int mediumPizzaSlices = 6;
+
+                                int totalSlicesNeeded = (superHungryPeopleForMediumPizza * slicesPerSuperHungry) +
+                                        (hungryPeopleForMediumPizza * slicesPerHungry) +
+                                        (notSoHungryPeopleForMediumPizza * slicesPerNotSoHungry);
+
+                                int mediumPizzaBoxes = (totalSlicesNeeded + mediumPizzaSlices - 1) / mediumPizzaSlices;
+                                int remainingSlices = mediumPizzaSlices - slicesPerSuperHungry - slicesPerHungry - slicesPerNotSoHungry;
+
+                                System.out.println("You will need about " + mediumPizzaBoxes + " boxes of pizza. \n");
+                                System.out.println("Approximately " + remainingSlices + " slices of pizza will remain.");
+                            }
+                        }
+                    } else if (numberOfGuestForMediumPizza < 0) {
+                        System.out.println("Give a valid number of guests!. \n Try Again.\n");
+                    }
+
+                } else if (sizeOfPizza.equalsIgnoreCase("Small")) {
+                    System.out.println("What number of guests are you expecting? ");
+                    int numberOfGuestsForSmallPizza = input.nextInt();
+
+                    if (numberOfGuestsForSmallPizza > 0) {
+                        System.out.println("How many guests are super hungry from the " + numberOfGuestsForSmallPizza + " guest you are expecting? ");
+                        int superHungryForSmallPizza = input.nextInt();
+
+                        if (superHungryForSmallPizza > 0 && superHungryForSmallPizza <= numberOfGuestsForSmallPizza) {
+                            System.out.println("How many slices will each super hungry guest eat? ");
+                            int slicesPerSuperHungry = input.nextInt();
+
+                            System.out.println("How many guests are hungry from the " + (numberOfGuestsForSmallPizza - superHungryForSmallPizza) + " guests left?");
+                            int hungryPeopleForSmallPizza = input.nextInt();
+
+                            if (hungryPeopleForSmallPizza >= 0 && hungryPeopleForSmallPizza <= (numberOfGuestsForSmallPizza - superHungryForSmallPizza)) {
+                                System.out.println("How many slices will each hungry guest eat?");
+                                int slicesPerHungry = input.nextInt();
+
+                                int notSoHungryPeopleForSmallPizza = numberOfGuestsForSmallPizza - superHungryForSmallPizza - hungryPeopleForSmallPizza;
+
+                                System.out.println("How many slices will each not-so-hungry guest eat?");
+                                int slicesPerNotSoHungry = input.nextInt();
+
+                                int SmallPizzaSlices = 4;
+
+                                int totalSlicesNeeded = (superHungryForSmallPizza * slicesPerSuperHungry) +
+                                        (hungryPeopleForSmallPizza * slicesPerHungry) +
+                                        (notSoHungryPeopleForSmallPizza * slicesPerNotSoHungry);
+
+
+                                int SmallPizzaBoxes = (totalSlicesNeeded + SmallPizzaSlices - 1) / SmallPizzaSlices;
+                                int remainingSlices = SmallPizzaSlices - slicesPerSuperHungry - slicesPerHungry - slicesPerNotSoHungry;
+                                ;
+
+                                System.out.println("You will need about " + SmallPizzaBoxes + " boxes of pizza. \n");
+                                System.out.println("Approximately " + remainingSlices + " slices of pizza will remain.");
+                            }
+                        }
+                    } else {
+                        System.out.println("Invalid Response. \n Try Again. ");
+                    }
+
+                }
+            }else if (response.equalsIgnoreCase("No")) {
+                    System.out.println("Alright! \n Have a nice day!! ");
+                    break;
+                } else {
+                    System.out.println("Invalid response! \n You are required to answer either (Yes or No)\n Try Again..\n");
+
+                }
+            }
+        }
+
+}
--- test/MyArrayPractice/ElementsOccurrenceTest.java
+++ test/MyArrayPractice/ElementsOccurrenceTest.java
@@ -10,8 +10,7 @@ class ElementsOccurrenceTest extends Account {
     public void elementsOccurrenceTest(){
         int[] numbers = {5, 2, 7, 8, 5, 3, 7, 2, 7, 5, 8, 8, 1};
         int[] occurrence = ElementsOccurrence.checkOccurrence(numbers);
-        int[] expected = {};
-        assertEquals(occurrence,expected);
-
+        int[] expected = {5, 1, 2, 1, 7, 1, 8, 1, 5, 1, 3, 1, 7, 1, 2, 1, 7, 1, 5, 1, 8, 1, 8, 1, 1, 1};
+        assertArrayEquals(occurrence,expected);
     }
 }
